1b7e15cb83187b91494c6f4dcdf2bf7e3ecb4991
Vijay-AndroidTest <vijayav64@gmail.com>
2026-04-18 22:31:12 +0530

    deployment error

--- next.config.mjs
+++ next.config.mjs
@@ -4,13 +4,6 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
-    // This ensures modern builds don't get stuck on old cache files
-    webpack: (config, { dev }) => {
-        if (config.cache && !dev) {
-            config.cache = false;
-        }
-        return config;
-    },
 };
 
 export default nextConfig;
